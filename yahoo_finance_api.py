835daa4bfa7f0a883123c6bc52c5808f26e88b48
Yogesh Aggarwal <yogesh30534@gmail.com>
2015-12-04 19:57:53 -0500

    ver0.5

--- yahoo_finance_api.py
+++ yahoo_finance_api.py
@@ -17,7 +17,49 @@ from os.path import isfile, join, getsize
 import networkx as nx
 from graph_tool.all import *
 import numpy as np
+import matplotlib.pyplot as plt
+from random import *
 
+
+config = SparkConf()
+#self.awsAccessKeyId="<awsAccessKeyId>"
+#self.awsSecretAccessKey="<awsSecretAccessKey>"
+#config.set("fs.s3n.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
+#config.set("fs.s3n.awsAccessKeyId", self.awsAccessKeyId)
+#config.set("fs.s3n.awsSecretAccessKey", self.awsSecretAccessKey)
+
+sc = SparkContext(conf=config)
+sqlContext = SQLContext(sc)
+
+
+def loadJsonDataAsPandasDF(filename):
+    with open(filename, 'rb') as f:
+        data = f.readlines()
+
+    # remove the trailing "\n" from each line
+    data = map(lambda x: x.rstrip(), data)
+
+    # each element of 'data' is an individual JSON object.
+    # i want to convert it into an *array* of JSON objects
+    # which, in and of itself, is one large JSON objectfr
+    # basically... add square brackets to the beginning
+    # and end, and have all the individual business JSON objects
+    # separated by a comma
+    data_json_str = "[" + ','.join(data) + "]"
+
+    # now, load it into pandas dataframe and return it
+    return pd.read_json(data_json_str)
+
+
+def loadJsonDataAsSparkDF(filename):
+	#df = sqlContext.read.json("s3n://"+self.awsAccessKeyId+":"+self.awsSecretAccessKey+"@"+self.bucketName+"/"+fileName)
+	df = sqlContext.read.json(filename)
+	return df
+
+def writePandasDataFrameAsJsonToFile(df, filename):
+	f = open(filename, "wb")
+	df.to_json(f, orient='records')
+	f.close()
 
 # Get list of companies for particular stock exchange
 # exchange: {'nasdaq', 'nyse', 'amex'}
@@ -131,15 +173,13 @@ def getSectorIndustryData():
 	joinedDF = industryDF.merge(sectorDF, on="industry_id") #, how="inner", lsuffix="_left", rsuffix="_right"
 	joinedDF = joinedDF.drop_duplicates()
 
-	f = open("sector_industry_company.json", "wb")
-	joinedDF.to_json(f, orient='records')
-	f.close()
+	writePandasDataFrameAsJsonToFile(joinedDF, "sector_industry_company.json")
 
 	return joinedDF
 
 
 def loadSectorIndustryData():
-	df = pd.DataFrame("sector_industry_company.json")
+	df = loadJsonDataAsSparkDF("sector_industry_company.json")
 	return df
 
 
@@ -248,21 +288,6 @@ def fetchData(symbols, startYear, fileLoc):
 # load all stock quotes from files in fileLoc in a spark dataframe
 # exception: stackOverflow error
 def loadData(fileLoc):
-	config = SparkConf()
-	#self.awsAccessKeyId="<awsAccessKeyId>"
-	#self.awsSecretAccessKey="<awsSecretAccessKey>"
-	#config.set("fs.s3n.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
-	#config.set("fs.s3n.awsAccessKeyId", self.awsAccessKeyId)
-	#config.set("fs.s3n.awsSecretAccessKey", self.awsSecretAccessKey)
-
-	sc = SparkContext(conf=config)
-	sqlContext = SQLContext(sc)
-
-	def loadJsonDataAsSparkDF(filename):
-	    #df = sqlContext.read.json("s3n://"+self.awsAccessKeyId+":"+self.awsSecretAccessKey+"@"+self.bucketName+"/"+fileName)
-	    df = sqlContext.read.json(filename)
-	    return df
-
 	#fileLoc = './data'
 	#fileList = listdir(fileLoc)
 	fileList = ['MSFT.json']
@@ -289,16 +314,19 @@ def loadData(fileLoc):
 	#quotes.show()  
 	return quotes 
 
-def gatherStats(fileLoc):
-	sc = SparkContext()
-	sqlContext = SQLContext(sc)
 
-	def loadJsonDataAsSparkDF(filename):
-	    df = sqlContext.read.json(filename)
-	    return df
+def gatherStats(fileLoc, sector=None):
+	if sector != None:
+		df = pd.read_json("nasdaq_sector_industry_company.json")
+		symbols = list(df[df['sector_name'] == sector]['company_symbol'])
+		fileList = [(symbol + ".json") for symbol in symbols]
+	else:
+		fileList = listdir(fileLoc)
+
+	print "fileListLen:", len(fileList)
 
 	stats = pd.DataFrame()
-	for f in listdir(fileLoc): 
+	for f in fileList: 
 		path = join(fileLoc, f)
 		if isfile(path) and getsize(path) > 0 and f!='dummy.json':
 			#print "loading data from file", f
@@ -320,20 +348,42 @@ def gatherStats(fileLoc):
 
 	return stats			
 
+
+# check number of records for each stock,
+# select those stocks for which number of records is equal 
+# to the number of records which most stocks have.
 # there are 1502 stocks with 2745 stocks
-def findStockSubsetToAnalyse(fileLoc):
-	stats = gatherStats(fileLoc)
-	grp = stats.groupby('Count').count()
-	maxRecords = grp.max(axis=1).index[0]
+def findStockSubsetToAnalyse(fileLoc, sector=None):
+	stats = gatherStats(fileLoc, sector)
+	#grp = stats.groupby('Count').count()
+	maxRecords = int(stats['Count'].value_counts().index[0])
+	print "maxRecords:", maxRecords
 	stats = stats[stats['Count'] == maxRecords]
 	stats.index = range(len(stats))
-	# print stats.count()
-	stats['Symbol'].to_csv('stock_subset.csv', index=False)
+	print "numStocksWithMaxRecords:", stats.count()
+
+	if sector != None:
+		filename = "stock_subset_"+sector+".csv"
+	else:
+		filename = "stock_subset.csv"
+
+	stats['Symbol'].to_csv(filename, index=False)
 
 	return stats['Symbol']
 
-def loadStockSubsetToAnalyse(fileLoc):
-	symbols = pd.read_csv('stock_subset.csv', names=['Symbol'])['Symbol']
+
+def loadStockSubsetToAnalyse(fileLoc, sector=None):
+	if sector != None:
+		filename = "stock_subset_"+sector+".csv"
+		outFilename = "closing_price_subset_"+sector+".json"
+	else:
+		filename = "stock_subset.csv"
+		outFilename = "closing_price_subset.json"
+
+	print "input file name:", filename
+	print "output file name:", outFilename
+
+	symbols = pd.read_csv(filename, names=['Symbol'])['Symbol']
 	quotes = pd.DataFrame()
 
 	for symbol in symbols:
@@ -345,26 +395,40 @@ def loadStockSubsetToAnalyse(fileLoc):
 			quotes[symbol] = temp['Close'] 
 			#break
 
-	quotes.to_json('closing_price_subset.json')
+	quotes.to_json(outFilename)
 	#print quotes.columns
 	return quotes
 
 # not tested
-def calculateCorrelationMatrix(quotes):
-	quotes = pd.read_json('closing_price_subset.json')
+def calculateCorrelationMatrix(sector=None):
+	if sector != None:
+		filename = "closing_price_subset_"+sector+".json"
+		outFilename = "corr_matrix_"+sector+".json"
+	else:
+		filename = "closing_price_subset.json"
+		outFilename = "corr_matrix.json"
+
+	quotes = pd.read_json(filename)
 	corrMat = quotes.corr(method='pearson')
 
-	corrMat.to_json('corr_matrix.json')
+	corrMat.to_json(outFilename)
 	return corrMat
 
 # using networkx lib
-def createGraphNX(threshold=0.5):
-	corrMat = pd.read_json('corr_matrix.json')
+def createGraphNXOld(threshold=0.5, sector=None):
+	if sector != None:
+		filename = "corr_matrix_"+sector+".json"
+		outFilename = "stock_graph_nx_"+sector+".xml"
+	else:
+		filename = "corr_matrix.json"
+		outFilename = "stock_graph_nx.xml"
+
+	corrMat = pd.read_json(filename)
 	#print corrMat.head()
 	symbols = corrMat.columns
 	numStocks = len(symbols)
 	
-	stockGraph = nx.Graph()
+	stockGraph = nx.Graph()	
 	for i in range(numStocks):
 		for j in range(i+1, numStocks):
 			w = corrMat[symbols[i]][symbols[j]]
@@ -373,13 +437,61 @@ def createGraphNX(threshold=0.5):
 				stockGraph.add_edge(symbols[i], symbols[j], weight=float(w))
 				#stockGraph.add_edge(symbols[i], symbols[j])
 
-	nx.write_graphml(stockGraph, "stock-graph-nx.xml")
+	# put all nodes in same cluster
+	nx.set_node_attributes(stockGraph, 'cluster', 0)
+
+	nx.write_graphml(stockGraph, outFilename)
 	#print stockGraph.graph
 	return stockGraph
 
+
+# using networkx lib
+def createGraphNX(threshold=0.5, sector=None):
+	if sector != None:
+		filename = "corr_matrix_"+sector+".json"
+		outFilename = "stock_graph_nx_"+sector+".xml"
+	else:
+		filename = "corr_matrix.json"
+		outFilename = "stock_graph_nx.xml"
+
+	sectors = pd.read_json("nasdaq_sector_industry_company.json")
+	industry = sectors[sectors['sector_name'] == sector]
+	company = dict(zip(industry['company_symbol'], industry['company_name']))
+
+	corrMat = pd.read_json(filename)
+	#print corrMat.head()
+	symbols = corrMat.columns
+	numStocks = len(symbols)
+	
+	stockGraph = nx.Graph()
+	for i,sym in enumerate(symbols):
+		cluster = 0 #randint(1, 5)
+		companyName = company.get(sym)
+		stockGraph.add_node(i, symbol=sym, name = companyName, cluster=cluster)	
+
+	for i in range(numStocks):
+		for j in range(i+1, numStocks):
+			w = corrMat[symbols[i]][symbols[j]]
+			if abs(w) >= threshold:
+				#print "adding edge: (", symbols[i], ",", symbols[j], ",", w, ")" 
+				stockGraph.add_edge(i, j, weight=float(w))
+				#stockGraph.add_edge(symbols[i], symbols[j])
+
+	nx.write_graphml(stockGraph, outFilename)
+	#print stockGraph.graph
+	return stockGraph
+
+
 # using graph-tool lib
-def createGraphGT(threshold=0.5):
-	corrMat = pd.read_json('corr_matrix.json')
+def createGraphGT(threshold=0.5, sector=None):
+	if sector != None:
+		filename = "corr_matrix_"+sector+".json"
+		outFilename = "stock_graph_gt_"+sector+".xml"
+	else:
+		filename = "corr_matrix.json"
+		outFilename = "stock_graph_gt.xml"
+
+	corrMat = pd.read_json(filename)
 	#print corrMat.head()
 	symbols = corrMat.columns
 	numStocks = len(symbols)
@@ -415,13 +527,20 @@ def createGraphGT(threshold=0.5):
 	#	g.vp.symbol[g.vertex(v)] = symbols[v]
 
 	#print g.num_vertices(), g.num_edges()
-	g.save("stock-graph-gt.xml", fmt="graphml")
+	g.save(outFilename, fmt="graphml")
 
 	return g
 
 # draw graph created using createGraphGT()
-def drawGraph():
-	g = load_graph("stock-graph-gt.xml")
+def drawGraphGT(sector=None):
+	if sector != None:
+		filename = "stock_graph_gt_"+sector+".xml"
+		outFilename = "stock_graph_gt"+sector+".pdf"
+	else:
+		filename = "stock_graph_gt.xml"
+		outFilename = "stock_graph_gt.pdf"
+
+	g = load_graph(filename)
 	#g = price_network(1500)
 
 	#g = GraphView(gg, vfilt=lambda v: v.out_degree() > 0)
@@ -450,17 +569,25 @@ def drawGraph():
 	graph_draw(g, pos=pos, vertex_text=g.vp.symbol, vertex_size=deg, vertex_fill_color=deg, \
 		vertex_font_size=deg, vorder=deg, \
 		edge_color=ebet, eorder=eorder, edge_pen_width=ebet, edge_control_points=control, \
-		output="stock-graph.pdf")
+		output=outFilename)
 
 
-def drawGraph1():
-	g = load_graph("stock-graph-gt.xml")
+def drawGraphGT1(sector=None):
+	if sector != None:
+		filename = "stock_graph_nx_"+sector+".xml"
+		outFilename = "stock_graph_nx_"+sector+".pdf"
+	else:
+		filename = "stock_graph_nx.xml"
+		outFilename = "stock_graph_nx.pdf"
+
+	g = load_graph(filename)
+	#g.save("temp_gt.xml", fmt="graphml")
 
 	#g = GraphView(gg, vfilt=lambda v: v.out_degree() > 0)
 	print g.num_vertices(), g.num_edges()
 
 	deg = g.degree_property_map("in")
-	deg.a = 4 * (np.sqrt(deg.a) * 0.5 + 0.4)
+	deg.a = 10 * (np.sqrt(deg.a) * 0.5 + 0.4)
 	#print "degree len:", len(deg.a)
 
 	ebet = betweenness(g)[1]
@@ -479,10 +606,53 @@ def drawGraph1():
 		d = np.sqrt(sum((	pos[e.source()].a - pos[e.target()].a) ** 2)) / 5
 		control[e] = [0.3, d, 0.7, d]
 		
-	graph_draw(g, pos=pos, vertex_text=g.vp.symbol, vertex_size=deg, vertex_fill_color=deg, \
+	graph_draw(g, pos=pos, vertex_text=g.vp.symbol, \
+		vertex_size=deg, vertex_fill_color=g.vp.cluster, \
 		vertex_font_size=deg, vorder=deg, \
 		edge_color=ebet, eorder=eorder, edge_control_points=control, \
-		output="stock-graph.pdf")
+		output=outFilename)
+
+
+# TODO - incomplete code
+def drawGraphNX(G, graph_layout='spring'):
+    # these are different layouts for the network you may try
+    # shell seems to work best
+    if graph_layout == 'shell':
+    	graph_pos=nx.spring_layout(G)
+    elif graph_layout == 'spectral':
+        graph_pos=nx.spectral_layout(G)
+    elif graph_layout == 'random':
+        graph_pos=nx.random_layout(G)
+    else:
+        graph_pos=nx.spring_layout(G)
+
+    deg = G.degree().values()
+    node_size = [d*100 for d in deg] #nx.eigenvector_centrality_numpy(G, weight="weight").values()
+    node_alpha = 0.3
+    node_color = nx.get_node_attributes(G, 'cluster').values()
+    edge_thickness = 1
+    edge_alpha = 0.3
+    edge_color = 'blue'
+    node_text_size = node_size
+    text_font = 'sans-serif'
+    edge_text_pos=0.3
+	
+	#if labels is None:
+	#	labels = range(len(graph))
+
+    #edge_labels = dict(zip(graph, labels))
+
+    # draw graph
+    nx.draw_networkx_nodes(G,graph_pos,node_size=node_size, 
+                           alpha=node_alpha, node_color=node_color)
+    nx.draw_networkx_edges(G,graph_pos,width=edge_thickness,
+                           alpha=edge_alpha,edge_color=edge_color)
+    #nx.draw_networkx_labels(G, graph_pos,font_size=node_text_size, font_family=text_font)
+    #nx.draw_networkx_edge_labels(G, graph_pos, edge_labels=edge_labels, label_pos=edge_text_pos)
+
+    #plt.savefig("something.pdf")
+    # show graph
+    plt.show()
 
 
 def findFreqOfCliquesInGraph(g):
@@ -498,6 +668,7 @@ def findFreqOfCliquesInGraph(g):
 	return freq
 
 
+# Plot histogram from dictionary object
 def plotHistFromDict(x):
 	import matplotlib.pyplot as plt
 
@@ -507,3 +678,16 @@ def plotHistFromDict(x):
 	plt.xticks(range(len(x)), x.keys())
 
 	plt.show()
+
+
+# temporary function
+def tempCliques():
+	#g = createGraphNX(0.8, 'Technology')
+	g = nx.read_graphml("stock_graph_nx_Technology.xml")
+	#freq = findFreqOfCliquesInGraph(g)
+	#plotHistFromDict(freq)
+	
+	comm = nx.k_clique_communities(g, 5)
+	communities = []
+	for c in comm:
+		print c
